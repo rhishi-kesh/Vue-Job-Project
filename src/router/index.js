f2def387f66ffd910d5501a4e3097711e0cbd51a
Rhishi kesh <reshikash300@gmail.com>
2025-03-29 19:33:04 +0600

    Single job page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import notFountView from '@/views/notFountView.vue'
+import JobView from '@/views/JobView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,8 +18,13 @@ const router = createRouter({
     },
     {
       path: '/jobs',
-      name: 'job',
+      name: 'jobs',
       component: () => import('../views/Jobs.vue'),
+    },
+    {
+      path: '/jobs/:id',
+      name: 'job',
+      component: JobView
     },
     {
       path: '/:catchAll(.*)',
